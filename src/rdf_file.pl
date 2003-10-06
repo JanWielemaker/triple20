17242d325254b32810da5a2c3eac9c6712f3862a
Jan Wielemaker <wielemak@science.uva.nl>
2003-10-06 15:03:58 +0000

    * Remove an OntoShow

--- src/rdf_file.pl
+++ src/rdf_file.pl
@@ -44,7 +44,7 @@
 rdf_file_extension(rdf,  'RDF file').
 rdf_file_extension(rdfs, 'RDF Schema file').
 rdf_file_extension(owl,  'OWL ontology file').
-rdf_file_extension(rdfj, 'OntoShow project file').
+rdf_file_extension(rdfj, 'Triple20 project file').
 
 		 /*******************************
 		 *	    SNAPSHOTS		*
